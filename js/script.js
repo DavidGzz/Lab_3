6fd4312165df22a9db4f0a9fb96e9138aa7d15c8
David González <david_gonzalez_97@hotmail.com>
2020-07-13 17:55:18 -0500

    Functioning

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
+
+// Paso #1
 let post = document.getElementById("ButtonPost");
 let clear = document.getElementById("ButtonClear");
 let mark = document.getElementById("ButtonMark");
 let del = document.getElementById("ButtonDelete");
 
+// Paso #2
 post.addEventListener("click", TodoPost);
 clear.addEventListener("click", TodoClear);
 mark.addEventListener("click", TodoMark);
 del.addEventListener("click", TodoDel);
-
-
-
-
 
 function TodoPost(e){
     e.preventDefault();
@@ -18,20 +17,34 @@ function TodoPost(e){
     var list = document.getElementById("todoList");
 
     let currentListHTML = list.innerHTML;
-    list.innerHTML = currentListHTML + '<input type = "checkbox" name = "todo" />' + todo + '<br>';
+    list.innerHTML = currentListHTML + '<input type = "checkbox" name = "todo" />' + " " + todo + '<br>';
 }
 
 function TodoClear(e){
     e.preventDefault();
+    var todos = document.getElementsByName("todo");
+
+    for(var i = 0; i < todos.length; i++){
+        todos[i].checked = false;
+    }
 
 }
 
 function TodoMark(e){
     e.preventDefault();
+    var todos = document.getElementsByName("todo");
+
+    for(var i = 0; i < todos.length; i++){
+        todos[i].checked = true;
+    }
+
 
 }
 
 function TodoDel(e){
     e.preventDefault();
+
+    var list = document.getElementById("todoList");
+    list.innerHTML = "";
 
 }
